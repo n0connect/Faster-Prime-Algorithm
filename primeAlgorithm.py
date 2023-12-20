311a36358b4c8e9fed328835d3bce02e61d40f18
n0connect <126422643+n0connect@users.noreply.github.com>
2023-12-20 13:43:21 +0300

    Update primeAlgorithm.py

--- primeAlgorithm.py
+++ primeAlgorithm.py
@@ -13,11 +13,21 @@ def take_user_number() -> None:
     """)
     try:
         selected_number: int = int(input("Enter a number (number>=2000): "))
-        load_prime_list(selected_number)
+        check_last_saved_file(selected_number)
     except ValueError as er:
         raise print(f"{er}")
+    except Exception as ex:
+        raise print(f"{ex}")
 
     return None
+
+
+def information_about_last_saved_file(loaded_list: list) -> None:
+    len_of_list = len(loaded_list)
+    print(f"The prime number at the top of the list: {loaded_list[0]}")
+    print(f"The prime number in the middle of the list: {loaded_list[int(len_of_list/2)]}")
+    print(f"The prime number at the end of the list: {loaded_list[-1]}")
+    print(f"the list contains {len_of_list} prime numbers")
 
 
 def look_in_terminal(output_list: list) -> None:
@@ -26,40 +36,56 @@ def look_in_terminal(output_list: list) -> None:
         counter = counter + 1
         print(item, end="\n" if counter % 10 == 1 else ",")
 
-    return print("\nDone_Success_w_in_terminal")
+    return print("\nDone_Success_look_in_terminal")
 
 
-def process_bar(number,user_numbers):
+def process_bar(number, user_numbers):
     temp_time: int = 100 * ((number + 2) / user_numbers)
-    sys.stdout.write(f"\rProgress: [{'=' * int(temp_time*0.5)}] {temp_time:.2f}%")
+    sys.stdout.write(f"\rProgress: [{'=' * int(temp_time * 0.5)}] {temp_time:.2f}%")
     sys.stdout.flush()
 
 
 def prime_control(user_numbers, default_prime_list: list):
-
     for number in range(default_prime_list[-1], user_numbers, 2):
+
         for prime_item in default_prime_list:
             if number % prime_item == 0:
                 process_bar(number, user_numbers)
                 break
+
         else:
             default_prime_list.append(number)
+            save_prime_list(default_prime_list)
 
     default_prime_list.insert(0, 2)
-    save_prime_list(default_prime_list)
+
     look_in_terminal(default_prime_list)
-    
+
     return None
 
 
 def save_prime_list(save_list: list):
-    with open('saved_prime_list.pkl', 'wb') as temp_list:
-        pickle.dump(save_list, temp_list)
+    try:
+        with open('saved_prime_list.pkl', 'wb') as temp_list:
+            pickle.dump(save_list, temp_list)
+    except Exception as ex:
+        raise print(f"{ex}")
 
     return None
 
 
-def load_prime_list(user_number) -> list or None:
+def load_prime_list(user_number, file_name) -> list or None:
+    print(f"{file_name} file exists.")
+
+    with open('saved_prime_list.pkl', 'rb') as saved_file:
+        loaded_list = pickle.load(saved_file)
+        print(f"{file_name}\n\nPreparing...")
+        information_about_last_saved_file(loaded_list)
+        time.sleep(3)
+        prime_control(user_number, loaded_list)
+
+
+def check_last_saved_file(user_number) -> object or None:
     try:
         current_directory = os.getcwd()
 
@@ -68,18 +94,10 @@ def load_prime_list(user_number) -> list or None:
     except Exception as ex:
         raise print(f"{ex}")
 
-    if os.path.exists(file_path):
-        print(f"{file_name} file exists.")
+    if not os.path.exists(file_path):
+        raise FileNotFoundError
 
-        with open('saved_prime_list.pkl', 'rb') as saved_file:
-            loaded_list = pickle.load(saved_file)
-            print(f"{file_name} last number in list: {loaded_list[-1]}\nPreparing...")
-            time.sleep(5)
-            prime_control(user_number, loaded_list)
-
-    else:
-        print(f"{file_name} file does not exist, program is closing...")
-        sys.exit(1)
+    load_prime_list(user_number, file_name)
 
 
 take_user_number()
